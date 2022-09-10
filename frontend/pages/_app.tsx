19a727085804db2e13911441ee6534d621b5fee8
fanzru <fattahilaaf080701@gmail.com>
2022-09-10 19:57:44 +0700

    feat(index): init maintenance pages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Script from 'next/script';
+import { NextSeo } from 'next-seo';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <NextSeo
+      title="Fanzru - Final Project University"
+      description="Jangan lupa berusahaan walaupun belum tentu berhasil karena menyerah sama saja dengan kalah."
+      openGraph={{
+        type: 'website',
+        url: 'https://skripsi.fanzru.dev/',
+        title: 'Fanzru - Final Project University',
+        description:
+          'Jangan lupa berusahaan walaupun belum tentu berhasil karena menyerah sama saja dengan kalah',
+        images: [
+          {
+            url: '/default.jpeg',
+          },
+        ],
+      }}
+    />
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
