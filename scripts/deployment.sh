6db03e4b038b7f480dfccfb36a9c309533728f20
fanzru <fattahilaaf080701@gmail.com>
2022-09-10 20:05:18 +0700

    feat(bash): script to deployment in vm

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,6 +1,39 @@
+#!/bin/sh
+set -e
+
+
 echo "================================================================"
 echo "===========================DEPLOYMENT==========================="
 echo "================================================================"
 
+echo "Update codebase..."
+cd ~/skripsi/final-project-university
+git fetch origin main
+git reset --hard origin/main
 
-echo "todo...."
+# echo "Moving to backend folder"
+# cd ~/skripsi/final-project-university/backend
+
+# echo "Installing dependencies 🛠"
+# go mod tidy
+
+# echo "Restart pm2 service backend 🔥"
+# pm2 restart deploy.json
+
+# echo "Deploying Backend Application Successfully Yeayyyy ......."
+
+echo "========================= FRONT END ========================="
+
+echo "Moving to frontend folder"
+cd ~/skripsi/final-project-university/frontend
+
+echo "Installing dependencies 🛠"
+yarn install
+
+echo "Building application ⚙"
+yarn build
+
+echo "Restart pm2 service 🔥"
+pm2 restart pm2.json
+
+echo "Deploying Frontend Application Successfully Yeayyyy ........"
