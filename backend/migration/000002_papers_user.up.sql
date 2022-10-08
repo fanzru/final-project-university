0ffd225bf39d53134711cf0e29b2f9c2e1063820
fanzru <fattahilaaf080701@gmail.com>
2022-10-08 17:04:04 +0700

    feat(migration): add table to save paper after parse from grobid

---
+++ backend/migration/000002_papers_user.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `papers_user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11),
+  `paper_name` LONGTEXT,
+  `link_pdf` LONGTEXT,
+  `is_done` BOOLEAN,
+  `created_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_users_papers` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+);
