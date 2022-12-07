e51ee36e24c97fdfb1325cb01070b8942d8e0585
fanzru <fattahilaaf080701@gmail.com>
2022-12-08 00:08:09 +0700

    feat: integration api t5

---
+++ frontend/pages/feature/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Feature : NextPage = () => {
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Feature
---
+++ frontend/pages/summary/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import SummaryComponent from "../../components/summary"
+const Feature : NextPage = () => {
+  
+  return (
+    <div>
+      <SummaryComponent/>
+    </div>
+  )
+}
+
+export default Feature
